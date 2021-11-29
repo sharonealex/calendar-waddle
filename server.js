cf3681fba0a0df29a888fe78e86a92e8180d5175
Sharon <sharonelizalex@gmail.com>
2021-11-29 13:00:16 +1100

    added server file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes")
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listenerCount(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
