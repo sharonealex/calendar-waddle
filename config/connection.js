3aaf0b935424384eb2cdd525981f9d2da6f26fb9
Sharon <sharonelizalex@gmail.com>
2021-12-10 12:48:54 +1100

    added linting packages

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+require('dotenv').config();
+
+let sequelize;
+
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            port: '3306',
+            dialect: 'mysql'
+        }
+    );
+}
+
+
+module.exports = sequelize;
